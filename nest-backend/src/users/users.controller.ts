4a8d95829f2023bc96c50a9cdda48e89fdbeae24
Edward3635 <Edgard3635@gmail.com>
2024-03-05 00:40:07 +0200

    new roles, dto and service

--- nest-backend/src/users/users.controller.ts
+++ nest-backend/src/users/users.controller.ts
@@ -2,6 +2,10 @@ import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UsersService } from './users.service'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { Roles } from 'src/auth/roles-auth.decorator'
+import { RolesGuard } from 'src/auth/roles.guard'
+import { AddRoleDto } from './dto/add-role.dto'
+import { BanRoleDto } from './dto/ban-role.dto'
 
 @Controller('users')
 export class UsersController {
@@ -11,10 +15,24 @@ export class UsersController {
 	create(@Body() userDto: CreateUserDto) {
 		return this.usersService.createUser(userDto)
 	}
-
-	@UseGuards(JwtAuthGuard)
+	@Roles('ADMIN')
+	@UseGuards(RolesGuard)
 	@Get()
 	getAll() {
 		return this.usersService.getAllUsers()
+	}
+
+	@Roles('ADMIN')
+	@UseGuards(RolesGuard)
+	@Post('/role')
+	addRole(@Body() dto: AddRoleDto) {
+		return this.usersService.addRole(dto)
+	}
+
+	@Roles('ADMIN')
+	@UseGuards(RolesGuard)
+	@Post('/ban')
+	ban(@Body() dto: BanRoleDto) {
+		return this.usersService.ban(dto)
 	}
 }
--- nest-backend/src/users/users.service.ts
+++ nest-backend/src/users/users.service.ts
@@ -1,8 +1,10 @@
 import { RolesService } from './../roles/roles.service'
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { User } from './users.model'
 import { InjectModel } from '@nestjs/sequelize'
 import { CreateUserDto } from './dto/create-user.dto'
+import { AddRoleDto } from './dto/add-role.dto'
+import { BanRoleDto } from './dto/ban-role.dto'
 
 @Injectable()
 export class UsersService {
@@ -23,6 +25,25 @@ export class UsersService {
 
 	async getUserByEmail(email: string) {
 		const user = await this.userRepositoty.findOne({ where: { email }, include: { all: true } })
+		return user
+	}
+	async addRole(dto: AddRoleDto) {
+		const user = await this.userRepositoty.findByPk(dto.userId)
+		const role = await this.RolesService.getRoleByValue(dto.value)
+		if (role && user) {
+			await user.$add('role', role.id)
+			return dto
+		}
+		throw new HttpException('Role or user were not found', HttpStatus.NOT_FOUND)
+	}
+	async ban(dto: BanRoleDto) {
+		const user = await this.userRepositoty.findByPk(dto.userId)
+		if (!user) {
+			throw new HttpException('User was not found', HttpStatus.NOT_FOUND)
+		}
+		user.banned = true
+		user.banReason = dto.banReason
+		await user.save()
 		return user
 	}
 }
