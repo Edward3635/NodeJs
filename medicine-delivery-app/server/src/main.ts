889122541b482527f0587696ddec17b33508de03
Edward3635 <Edgard3635@gmail.com>
2024-03-08 22:56:42 +0200

    deploy test

--- medicine-delivery-app/server/src/main.ts
+++ medicine-delivery-app/server/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module'
 
 async function start() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(3000, ()=>console.log('WO-hoo')
-	)
+	await app.listen(3000)
 }
 start()
