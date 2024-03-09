0026b921a5915a7340acfd00df244ad85a9dda66
Edward3635 <Edgard3635@gmail.com>
2024-03-09 13:05:19 +0200

    deploy test

--- medicine-delivery-app/server/src/main.ts
+++ medicine-delivery-app/server/src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-// import 'dotenv/config'
+import 'dotenv/config'
 
 async function start() {
 	const app = await NestFactory.create(AppModule)
 	const PORT = process.env.PORT || 5000
-	await app.listen(PORT)
+	await app.listen(PORT, () => console.log('port: ', process.env.PORT))
 }
 start()
