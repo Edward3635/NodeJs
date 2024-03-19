ac181a2e0d025b2e68d3fad8dff460d32ff2a4dd
Edward3635 <Edgard3635@gmail.com>
2024-03-19 13:11:57 +0200

    update server link url

--- medicine-delivery-app/client/src/api/api.js
+++ medicine-delivery-app/client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'http://localhost:5000/'
+	baseURL: 'https://nodejs-production-9965.up.railway.app/'
 	// baseURL:'https://nodejs-production-9965.up.railway.app/'
 })
 
